3718f7946d1bf08889a715cf67a54dd0ad4eb4cf
surpreet.mundra <surpreet.mundra@gmail.com>
2018-10-19 19:38:55 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,4 +6,10 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
 
+  def list_roles
+    self.characters.collect do |c|
+      "#{c.name} - #{c.show}"
+    end
+  end
+
 end
